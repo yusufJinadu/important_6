2972a85fc181d1d8b1f93d0add36696c47257868
yusufjinadu <y_jinadu@yahoo.com>
2023-07-23 10:48:14 +0200

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 function run(){
     const bucket = core.getInput('bucket', {required: true})
-    const bucketRegion = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('region', {required: true})
     const distFolder = core.getInput('dist-folder', {required: true})
     const s3Uri = `s3://${bucket}`
 
