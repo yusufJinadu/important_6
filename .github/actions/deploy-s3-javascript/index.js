00e80d4377ff5df7d6e3da3866c34f3898ede31a
yusufjinadu <y_jinadu@yahoo.com>
2023-07-25 20:14:48 +0200

    added custom deploy action

---
+++ .github/actions/deploy-s3-javascript/index.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+function run(){
+    core.setOutput('website-url','bololo')
+}
+
+run()
